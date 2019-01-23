f8a6bcc2fde6e260f7cb658ccbb442802ea2b2d3
olympiawojcik <owojcik7@gmail.com>
2019-01-23 10:16:33 -0500

    complete closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -12,12 +12,30 @@ returnGreet('Sup');
 
 
 // ==== Challenge 2: Create a counter function ====
+
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {
+    count++;
+    return count;
+  }
 };
+
+const newCounter = counter();    //newCounter is our anonymous function
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+
+//when we invoke counter, we return the invocaiton of an anonymous environment.
+// This would not be possible without closures.
+
+//function creates a closure between context of variable and new updated value of that variable.
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
